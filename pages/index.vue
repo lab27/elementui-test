ce9afcd59bceb6bf1165d00940e5d886d64d3f77
Mikhail Delport <mikhail@delport.ca>
2017-05-04 20:07:29 -0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <el-date-picker
+      v-model="date"
+      type="date"
+      placeholder="Pick a day"
+      align="left"
+      ref="eldatepicker">
+    </el-date-picker>
+  </section>
+</template>
+
+<script>
+import Logo from '~components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+
+  data () {
+    return {
+      date: ''
+    }
+  }
+}
+</script>
